592358e6950141ab8959f4680bf01a67ad960061
VanesaTaipe <vanesa.morales@upch.pe>
2025-11-30 23:30:13 -0500

    permiso

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",  # Frontend Angular
         "http://localhost:8000",  # Backend PHP (si es necesario)
+          "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
